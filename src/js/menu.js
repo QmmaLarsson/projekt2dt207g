1891d64c505606d0f5422f82d8c179cd3a4cf1f0
Emma Larsson <emma940112@gmail.com>
2024-05-12 09:31:39 +0200

    Gjort så att delete-knappen fungerar, man måste ha ett jwt-token för att kunna radera ett item

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,7 +1,7 @@
 "use strict";
 
 //Funktion för att hämta jobb från APIet
-async function getJobs() {
+async function getMenu() {
     const url = "https://projektdt207g.onrender.com/api/menu";
 
     const response = await fetch(url);
@@ -10,47 +10,79 @@ async function getJobs() {
 
     console.log(data);
 
-    displayJobs(data);
+    displayMenu(data);
 }
 
 //Funktion för att skriva ut alla job
-function displayJobs(jobs) {
-    const jobContainer = document.querySelector(".jobContainer");
+function displayMenu(items) {
+    const itemContainerEl = document.querySelector(".itemContainer");
 
     //Loopa igenom varje jobb och skapa ett HTML-element
-    jobs.forEach(job => {
-        const jobElement = document.createElement("div");
+    items.forEach(item => {
+        const itemElement = document.createElement("div");
 
         //Lägg till en klass till varje element
-        jobElement.classList.add("jobElement");
+        itemElement.classList.add("itemElement");
 
-        jobElement.innerHTML = `
-        <p><strong>Företagsnamn:</strong> ${job.name}</p>
-        <p><strong>Titel:</strong> ${job.type}</p>
-        <p><strong>Plats:</strong> ${job.description}</p>
-        <p><strong>Beskrivning:</strong> ${job.price}</p>
+        itemElement.innerHTML = `
+        <p><strong>Namn:</strong> ${item.name}</p>
+        <p><strong>Typ:</strong> ${item.type}</p>
+        <p><strong>Beskrivning:</strong> ${item.description}</p>
+        <p><strong>Pris:</strong> ${item.price}</p>
         <button class="deleteBtn" type="button">Ta bort</button>
+        <button class="editBtn" type="button">Redigera</button>
 `;
 
         // Lägg till jobbets ID som ett data-attribut
-        jobElement.dataset.jobId = job._id;
+        itemElement.dataset.itemId = item._id;
 
-        jobContainer.appendChild(jobElement);
+        itemContainerEl.appendChild(itemElement);
 
-        //Variabel för delete-knapp
-        const deleteBtn = jobElement.querySelector(".deleteBtn");
+        //Variabler för knappar
+        const deleteBtn = itemElement.querySelector(".deleteBtn");
+        const editBtn = itemElement.querySelector(".editBtn");
 
         //Händelsehanterare för delete-knapp
         deleteBtn.addEventListener("click", () => {
-            const jobId = jobElement.dataset.jobId;
-            deleteJob(jobId);
-            //Tar bort jobbet från sidan när användaren klickar på knappen
-            jobElement.remove();
+            const token = localStorage.getItem("token");
+
+            if (token) {
+                const itemId = itemElement.dataset.itemId;
+                deleteItem(itemId);
+                //Tar bort jobbet från sidan när användaren klickar på knappen
+                itemElement.remove();
+            } else {
+                console.log("Du måste vara inloggad för att kunna ta bort ett item.")
+            }
         });
     });
 }
 
+//Funktion för att ta bort ett jobb från APIet
+async function deleteItem(itemId) {
+    let deleteUrl = `https://projektdt207g.onrender.com/api/menu/${itemId}`;
+    const token = localStorage.getItem("token");
+
+    try {
+        const response = await fetch(deleteUrl, {
+            method: "DELETE",
+            headers: {
+                "Authorization": "Bearer " + token,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            console.log("Ett item har tagits bort från databasen.");
+        } else {
+            console.error("Det gick inte att ta bort ett item från databasen.");
+        }
+    } catch (error) {
+        console.error("Ett fel uppstod vid hämtning av data:", error);
+    }
+}
+
 //Funktion som gör att funktionerna ovanför kör direkt när sidan laddas in
 window.onload = function () {
-    getJobs();
+    getMenu();
 };
